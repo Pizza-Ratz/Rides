a3fd1de4167e2e0bc381f48dba1273442e421aac
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-07-18 23:31:00 -0400

    populates default reducer state

--- src/store/reducers/stations.js
+++ src/store/reducers/stations.js
@@ -1,5 +1,5 @@
-import SubwayStations from '../../data/SubwayStations.geojson.json'
-import SubwayStops from '../../data/SubwayStops.json'
+import SubwayStations from "../../data/SubwayStations.geojson.json";
+import SubwayStops from "../../data/SubwayStops.json";
 
 export const transiterNYCSubway =
   "https://transiter.mta-music.nyc/systems/us-ny-subway";
@@ -12,90 +12,112 @@ export const actionTypes = {
 
 // mapping from station name to station data
 const stationFromName = SubwayStops.reduce((accum, stop) => {
-  if (!stop.id.match(/\d+$/)) return accum
-  let stopName = stop.name
+  if (!stop.id.match(/\d+$/)) return accum;
+  let stopName = stop.name;
   // put st/nd/rd/th after street numbers
   for (const match of stopName.matchAll(/(\d+) /g)) {
-    let [orig, number] = match
-    const lastDigit = number % 10
+    let [orig, number] = match;
+    const lastDigit = number % 10;
     if (lastDigit == 1) {
-      number = number + 'st'
+      number = number + "st";
     } else if (lastDigit == 2) {
-      number = number + 'nd'
+      number = number + "nd";
     } else if (lastDigit == 3) {
-      number = number + 'rd'
+      number = number + "rd";
     } else {
-      number = number + 'th'
+      number = number + "th";
     }
-    stopName = stopName.replaceAll(orig, `${number} `)
+    stopName = stopName.replaceAll(orig, `${number} `);
   }
   // convert Av -> Ave, Ft -> Fort
   stopName = stopName
-    .replaceAll(' Av', ' Ave')
-    .replaceAll('-', ' - ')
-    .replaceAll('Ft ', 'Fort ')
+    .replaceAll(" Av", " Ave")
+    .replaceAll("-", " - ")
+    .replaceAll("Ft ", "Fort ");
 
-  accum[stopName] = stop
-  return accum
-}, {})
+  accum[stopName] = stop;
+  return accum;
+}, {});
 
 // injects MTA station ID into each station feature
 const stationsWithId = { ...SubwayStations };
-stationsWithId.features = SubwayStations.features.map(f => {
-  const station = stationFromName[f.properties.name]
-  // console.log(station ? station : f.properties.name)
-  if (!station) return { wtf: true }
-  return {
-    ...f,
-    properties: {
-      ...f.properties,
-      id: station.id,
-    },
-    geometry: { ...f.geometry, coordinates: [...f.geometry.coordinates] }
-  }
-}).filter(s => s.properties && s.properties.id)
-
+stationsWithId.features = SubwayStations.features
+  .map((f) => {
+    const station = stationFromName[f.properties.name];
+    // console.log(station ? station : f.properties.name)
+    if (!station) return { wtf: true };
+    return {
+      ...f,
+      properties: {
+        ...f.properties,
+        id: station.id,
+      },
+      geometry: { ...f.geometry, coordinates: [...f.geometry.coordinates] },
+    };
+  })
+  .filter((s) => s.properties && s.properties.id);
 
 export const loadStationsAction = (data) => ({
   type: actionTypes.LOAD_STATIONS,
-  data
+  data,
 });
 
 export const markStart = (startId) => ({
   type: actionTypes.MARK_START,
-  startId
-})
+  startId,
+});
 
 export const markEnd = (endId) => ({
   type: actionTypes.MARK_END,
-  endId
-})
+  endId,
+});
 
 export const loadStations = () => {
-  return {data: stationsWithId}
-}
+  return { data: stationsWithId };
+};
 
-export const initialState = {};
+export const initialState = { data: stationsWithId };
 
 const stationReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.LOAD_STATIONS:
       return action.data;
     case actionTypes.MARK_START:
-      return {...state, data: {...state.data, features: state.data.features.map(station => {
-        if (station.properties && station.properties.id === action.startId) {
-          return {...station, properties: {...station.properties, start: true}}
-      } else return station
-    })}}
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          features: state.data.features.map((station) => {
+            if (
+              station.properties &&
+              station.properties.id === action.startId
+            ) {
+              return {
+                ...station,
+                properties: { ...station.properties, start: true },
+              };
+            } else return station;
+          }),
+        },
+      };
     case actionTypes.MARK_END:
-      return {...state, data: {...state.data, features: state.data.features.map(station => {
-        if (station.properties && station.properties.id === action.endId) {
-          return {...station, properties: {...station.properties, end: true}}
-      } else return station
-    })}}
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          features: state.data.features.map((station) => {
+            if (station.properties && station.properties.id === action.endId) {
+              return {
+                ...station,
+                properties: { ...station.properties, end: true },
+              };
+            } else return station;
+          }),
+        },
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default stationReducer
+export default stationReducer;
