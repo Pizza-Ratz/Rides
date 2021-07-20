caf96fe4749a501b49758102368f984b4b881775
devine strake <elemkey@gmail.com>
2021-07-20 10:54:41 -0400

    Animated marker (#36)
    * fixed logo
    
    * added logos
    
    * icons on start end and new style for music stations
    
    * reassigning starting and ending stations
    
    * a srt and end button that dont do anything
    
    * flexboxed the mf
    
    * tried to text align no go
    
    * custom marker installed colorwill need to change
    
    * added markerStyle to newMarker

--- src/components/SubwayStationsLayer.js
+++ src/components/SubwayStationsLayer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import L from "leaflet";
 import logo from "../../src/assets/images/logo.png";
+import startOrEndLogo from "../../src/assets/images/start-end-logo.png";
 import { GeoJSON, useMap } from "react-leaflet";
 import {
   GlobalStationDispatchContext,
@@ -9,16 +10,26 @@ import {
 import { loadStations, markStart, markEnd } from "../store/reducers/stations";
 import "../assets/stylesheets/components/_SubwayStationLayer.scss";
 
+// const meow = new Event("meow")
+
 const popUpStyle = {
   className: "popupCustom",
 };
 
+const TriangleKnocker = L.icon({
+  iconUrl: startOrEndLogo,
+  iconSize: [35, 35],
+});
+
 function stationToMarker(station, latlng) {
-  let className = `station ${station.properties.name} `;
+  let stationName = station.properties.name.replaceAll(" ", "*");
+  let objectId = station.properties.objectid;
+  let className = `station meow${stationName} ${objectId}`;
   className += Object.keys(station.properties.classList).join(" ");
 
   const markerStyle = {
     className,
+    stationName,
     color: "#BEC2CBB3",
     border: "white",
     riseOnHover: true,
@@ -26,14 +37,18 @@ function stationToMarker(station, latlng) {
     bubblingMouseEvents: true,
   };
 
-  if (station.properties.start) markerStyle.className += " starting";
-  if (station.properties.end) markerStyle.className += " ending";
-
   if (typeof window === "undefined") {
     return null;
   }
 
+  if (station.properties.start) markerStyle.className += " starting";
+  if (station.properties.end) markerStyle.className += " ending";
+
   const marker = new L.CircleMarker(latlng, markerStyle);
+
+  if (station.properties.start === true || station.properties.end === true) {
+    return new L.Marker(latlng, { icon: TriangleKnocker }, markerStyle);
+  }
 
   return marker;
 }
@@ -47,13 +62,20 @@ const SubwayStationsLayer = () => {
     // if it's a station that got clicked
     if (evt.originalEvent.target.classList.contains("station") && evt.latlng) {
       const targetClass = evt.originalEvent.target.className.baseVal;
-      const stationName = targetClass
-        .replace(/station.*/, "")
-        .replace(" - ", "-");
+      let stationName = targetClass.match(/meow([^\s]+)/)[1];
+      // let stationId = evt.originalEvent.target.classList[2]
+
+      stationName = stationName.replaceAll("*", " ");
+
       map.openPopup(
-        `<img src=${logo} alt="logo" width="100%" height="100%" /><div>${stationName}</div>`,
+        `<div class="buttonpop">
+        <img src=${logo} alt="logo" width="100%" height="100%" />
+        <div class="button-name">${stationName}</div>
+        <button class="lower-button">🅢🅡🅣</button> 
+        <button class="lower-button">🅔🅝🅓</button>
+        <div>`,
         evt.latlng,
-        popUpStyle
+        popUpStyle,
       );
     }
   }
